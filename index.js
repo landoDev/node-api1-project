4898c979c104fc9eda1f1971f779e6f24353c039
Landon Turner <lturner@LTurner-MBP.lan1>
2020-04-06 16:44:33 -0400

    posting to api

--- index.js
+++ index.js
@@ -16,9 +16,22 @@ server.use(express.json())
 
 server.post('/api/users', (req, res) =>{
     const newUser = req.body;
-    users.push(newUser)
-    res.status(200).json(newUser)
-})
+    if(newUser.name == "" || newUser.bio == ""){
+        return res.status(400).json({errorMessage: "Please provide name and bio for the user"}) 
+    } else {
+            newUser.id = shortid.generate();
+            console.log(newUser); 
+            users.push(newUser)
+            return res.status(200).json(users);
+    }
+    // THE BELOW CODE RESULTS IN AN ERROR EVEN THOUGH CODE WORKS CAN'T DO TERNARY WITH MULTIPLE CHECKS IN RESPONSE
+    // newUser.name == "" && newUser.bio == "" ?  res.status(400).json({errorMessage: "Please provide name and bio for the user"}) 
+    // :
+    // newUser.id = shortid.generate();
+    // console.log(newUser); 
+    // users.push(newUser)
+    // res.status(200).json(users);
+});
 
 const port = 5000;
-server.listen(port, () => console.log(`* Server running on ${port} **`))
+server.listen(port, () => console.log(`* Server running on ${port} **`));
